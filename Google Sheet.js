f98205aaf0a85aa975849db918b9303468eafb33
Utsav-Bajpai <utsavb736@gmail.com>
2025-05-27 14:58:25 +0530

    Form, exp. and ser. det.

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,16 +1,25 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbwT-GdhGoUOMnmG6VXoeD5OjA7GpYSeehSnheBSqmX43JGg-xSG8sBR__gm7znrIsZs/exec";
 const form = document.forms["contact_form"];
 
+function showPopup(id) {
+    document.getElementById(id).style.display = 'flex';
+}
+
+function closePopup(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    var formData = new FormData(form);
+    const formData = new FormData(form);
 
     fetch(scriptURL, { method: "POST", body: formData })
-        .then((response) => {
-            alert("Done", "Submitted Successfully.", "success");
+        .then(response => {
             form.reset();
+            showPopup('successPopup');
         })
-        .catch((error) => {
-            alert("Error", "Something went wrong. Please try again!", "error");
+        .catch(error => {
+            console.error(error);
+            showPopup('errorPopup');
         });
 });
